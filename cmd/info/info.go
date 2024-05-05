03594a6d9e37292f1736b1cff25b5a56885b00a6
mrkouhadi <teacherbryan@outlook.com>
2024-05-06 00:58:53 +0800

    added more info

--- cmd/info/info.go
+++ cmd/info/info.go
@@ -13,7 +13,10 @@ var InfoCmd = &cobra.Command{
 	Short: "info is a pallete that contains commands to get info of CPU, RAM, and DISK usage",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Please specify what command do you want. example: go-cli info disk")
+		fmt.Println("Please specify what command do you want. ")
+		fmt.Println(" - CPU usage: go-cli info cpu")
+		fmt.Println(" - RAM usage: go-cli info ram")
+		fmt.Println(" - DISK usage: go-cli info disk")
 	},
 }
 
